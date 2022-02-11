b6b2f40da063171c5b4095fa00c37de352211e72
Uğur YEŞİLBAŞ <uguryesilbas@outlook.com>
2022-02-11 18:30:04 +0300

    Update KeyHelper.cs

--- Helpers/KeyHelper.cs
+++ Helpers/KeyHelper.cs
@@ -5,7 +5,7 @@ namespace Helpers
 {
     public static class KeyHelper
     {
-        public static int GenerateRandomKey(int size = 6)
+        public static int GenerateRandomKey(int size = 10)
         {
             int minValue = Convert.ToInt32(1.ToString().PadRight(size, '0'));
             int maxValue = Convert.ToInt32(9.ToString().PadRight(size, '9'));
